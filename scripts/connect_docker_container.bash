3edc690db694f64ead2049a8131bfea94d2ae94c
brucechanjianle <jianle001@e.ntu.edu.sg>
2020-08-21 08:58:55 +0800

    update scripts

--- scripts/connect_docker_container.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This script start the docker container
-
-echo -e "Starting container image ubuntu16.04:ros"
-read -p "Container name: " CONTAINERNAME
-
-docker run -d -ti -v $(pwd)/..:/home --name $CONTAINERNAME --cap-add=SYS_PTRACE ubuntu16.04:ros
