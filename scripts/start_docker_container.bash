55342bf0c7a826f2c3a6a20622754a040364fc9e
CHAN JIAN LE <49647142+BruceChanJianLe@users.noreply.github.com>
2020-08-21 15:00:08 +0800

    Update start_docker_container.bash

--- scripts/start_docker_container.bash
+++ scripts/start_docker_container.bash
@@ -4,4 +4,4 @@
 echo -e "Starting container image ubuntu18.04:ros"
 read -p "Container name: " CONTAINERNAME
 
-docker run -d -ti -v $(pwd)/..:/root --name $CONTAINERNAME --cap-add=SYS_PTRACE ubuntu18.04:ros
+docker run -d -ti -v $(pwd)/..:/root/docker_mount --name $CONTAINERNAME --cap-add=SYS_PTRACE ubuntu18.04:ros
