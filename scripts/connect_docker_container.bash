a2c2a7fddc1b6ab445e24b47b1304ff902aa3bc6
CHAN JIAN LE <49647142+BruceChanJianLe@users.noreply.github.com>
2020-08-24 10:09:32 +0800

    Update connect_docker_container.bash

--- scripts/connect_docker_container.bash
+++ scripts/connect_docker_container.bash
@@ -1,7 +1,32 @@
 #!/bin/bash
-# This script connect thsi terminal to specify docker container
+# This script connect this terminal to specific docker container
+
+# Display all container's name
+echo "List of containers:"
+declare -a arr
+i=0
+
+# Make container name into an array
+containers=$(docker ps -a | awk '{if(NR>1) print$NF}')
+for container in $containers
+do
+    arr[$i]=$container
+    let "i+=1"
+done
+
+# Loop through name array
+let "i-=1"
+for j in $(seq 0 $i)
+do
+    echo $j")" ${arr[$j]}
+done
 
 # Obtain container name
 read -p "Container name to be connected: " CONTAINERNAME
 
-docker exec -ti $CONTAINERNAME env TERM=xterm-256color bash -l
+if [[ -z ${arr[$CONTAINERNAME]} ]]
+then
+    docker exec -ti $CONTAINERNAME env TERM=xterm-256color bash -l
+else
+    docker exec -ti ${arr[$CONTAINERNAME]} env TERM=xterm-256color bash -l
+fi
